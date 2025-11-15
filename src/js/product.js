5dbac7054d9fdd38a08edd9a95ee665cf9109d9d
emyudofia1 <emyudofia1@gmail.com>
2025-11-15 15:00:13 -0700

    Corrections

--- src/js/product.js
+++ src/js/product.js
@@ -15,25 +15,3 @@ productDetails.init();
  ** *************************************** */
 superScript();
 
-// new new new
-
-const gridBtn = document.getElementById("gridViewBtn");
-const listBtn = document.getElementById("listViewBtn");
-const productsContainer = document.getElementById("home-products");
-
-// DEFAULT LAYOUT = grid
-productsContainer.classList.add("product-grid");
-
-gridBtn.addEventListener("click", () => {
-  productsContainer.classList.add("product-grid");
-  productsContainer.classList.remove("product-list");
-  gridBtn.classList.add("active");
-  listBtn.classList.remove("active");
-});
-
-listBtn.addEventListener("click", () => {
-  productsContainer.classList.add("product-list");
-  productsContainer.classList.remove("product-grid");
-  listBtn.classList.add("active");
-  gridBtn.classList.remove("active");
-});
