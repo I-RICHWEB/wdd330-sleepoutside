08dcd8e7c9f0062518fc8d398bb2e5761baa63cb
I-RICHWEB <ize20002@byui.edu>
2025-11-15 18:35:54 +0100

    cccc

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,5 +1,5 @@
 // src/js/ProductDetails.mjs
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage } from './utils.mjs';
 
 export default class ProductDetails {
   constructor(productId, dataSource) {
@@ -11,33 +11,28 @@ export default class ProductDetails {
   async init() {
     // Get product details
     this.product = await this.dataSource.findProductById(this.productId);
-
+    
     // Render the product details
     this.renderProductDetails();
-
+    
     // Add event listener to Add to Cart button
-    document
-      .getElementById("addToCart")
-      .addEventListener("click", this.addProductToCart.bind(this));
+    document.getElementById('addToCart')
+      .addEventListener('click', this.addProductToCart.bind(this));
   }
 
   addProductToCart() {
     setLocalStorage(`${this.product.Id}`, this.product);
-    alert("Product added to cart!");
+    alert('Product added to cart!');
   }
 
   renderProductDetails() {
     document.querySelector("#productName").textContent = this.product.Name;
-    document.querySelector("#productNameWithoutBrand").textContent =
-      this.product.NameWithoutBrand;
+    document.querySelector("#productNameWithoutBrand").textContent = this.product.NameWithoutBrand;
     document.querySelector("#productImage").src = this.product.Image;
     document.querySelector("#productImage").alt = this.product.Name;
-    document.querySelector("#productFinalPrice").textContent =
-      `$${this.product.FinalPrice}`;
-    document.querySelector("#productColorName").textContent =
-      this.product.Colors[0].ColorName;
-    document.querySelector("#productDescriptionHtmlSimple").innerHTML =
-      this.product.DescriptionHtmlSimple;
+    document.querySelector("#productFinalPrice").textContent = `$${this.product.FinalPrice}`;
+    document.querySelector("#productColorName").textContent = this.product.Colors[0].ColorName;
+    document.querySelector("#productDescriptionHtmlSimple").innerHTML = this.product.DescriptionHtmlSimple;
     document.querySelector("#addToCart").dataset.id = this.product.Id;
   }
 }
