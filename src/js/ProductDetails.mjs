8fd1dadbd33fe92066fcb7a437a9f6b48dae0c7c
I-RICHWEB <ize20002@byui.edu>
2025-11-26 23:51:25 +0100

    discount original price.

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -29,14 +29,27 @@ export default class ProductDetails {
   }
 
   renderProductDetails() {
+    const hasDiscount =
+      this.product.SuggestedRetailPrice &&
+      Number(this.product.FinalPrice) <
+        Number(this.product.SuggestedRetailPrice);
+
+    // Safely format prices
+    const formatPrice = (p) => {
+      const n = Number(p);
+      return Number.isFinite(n) ? n.toFixed(2) : p;
+    };
+
     document.querySelector("#productName").textContent = this.product.Name;
     document.querySelector("#productNameWithoutBrand").textContent =
       this.product.NameWithoutBrand;
     document.querySelector("#productImage").src =
       this.product.Images.PrimaryLarge;
     document.querySelector("#productImage").alt = this.product.Name;
-    document.querySelector("#productFinalPrice").textContent =
-      `$${this.product.FinalPrice}`;
+    document.querySelector("#productFinalPrice").innerHTML = `
+        ${hasDiscount ? `<span class="original-price">$${formatPrice(this.product.SuggestedRetailPrice)}</span>` : ""}
+        $${formatPrice(this.product.FinalPrice)}
+      `;
     document.querySelector("#productColorName").textContent =
       this.product.Colors[0].ColorName;
     document.querySelector("#productDescriptionHtmlSimple").innerHTML =
